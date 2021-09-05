9dbdefeb010a8ee56cdeb6ebff521ada561af349
nbowd <bowdenn@oregonstate.edu>
2021-09-05 12:12:14 -0700

    game can now add and display players initial pieces

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
-import produce from 'immer';
+import produce, { current } from 'immer';
 import './Game.css'; 
+import diamond from './triangle.png'
 
 function Game() {
   const SIZE = 5
   const CELLS = 100
-  const generateEmptyGrid = () => Array.from({length: SIZE}).map(() => Array.from({length: SIZE}).fill(0));
+  const generateEmptyGrid = () => Array.from({length: SIZE}).map(() => Array.from({length: SIZE}).fill({player: null, level:0}));
   const [board, setBoard] = useState(generateEmptyGrid())
+  const [gameState, setGameState] = useState('UNFINISHED')
+  const [currentTurn, setCurrentTurn] = useState('x')
+  const [initialReady, setInitialReady] = useState({'x': 0, 'o': 0})
+
+  console.log(board)
 
   const setCellColor = (row, col) => {
     const colors = {
@@ -16,16 +22,72 @@ function Game() {
       3: 'green',
       4: 'black'
     }
-    return colors[board[row][col]]
+ 
+    return colors[board[row][col].level]
+  }
+
+  const setCellPlayer = (row, col) => {
+    const players = {
+      'x': 'pink',
+      'o': 'yellow'
+    }
+
   }
 
   const handleClick = (row, col) => {
-    const newBoard = produce(board, boardCopy => {
-      boardCopy[row][col]= (boardCopy[row][col] + 1)% 5
-    })
-    setBoard(newBoard)
+    if (initialReady[currentTurn] < 2) {
+      console.log(initialReady);
+      initialPlacement(row,col)
+    } else {
+      const newBoard = produce(board, boardCopy => {
+        boardCopy[row][col].level = (boardCopy[row][col].level + 1)% 5
+      })
+      setBoard(newBoard)
+      changeTurns()
+    }
   }
 
+ 
+
+  const changeTurns = () => {
+    console.log('changing turns');
+    if (currentTurn === 'x') {
+      setCurrentTurn('o')
+    } else if (currentTurn === 'o') {
+      setCurrentTurn('x')
+    }
+  }
+
+  const initialPlacement = (x, y) => {
+    
+    // Checks for pieces already in those positions
+    if (board[x][y].player) {
+      console.log('player there already');
+      return false
+    }
+
+    const newBoard = produce(board, boardCopy => {
+      console.log('adding player piece');
+      boardCopy[x][y].player = currentTurn
+    })
+    setBoard(newBoard)
+    setInitialReady(produce(initialReady, initialReadyCopy => {initialReadyCopy[currentTurn] = initialReadyCopy[currentTurn] + 1}))
+    
+    changeTurns()
+    return true
+  }
+
+  const checkValidMove = (xStart, yStart, xFinish, yFinish) => {
+    const xChange = Math.abs(xFinish - xStart)
+    const yChange = Math.abs(yFinish - yStart)
+
+    if (xChange > 1 || yChange > 1) {
+      return false
+    }
+    if (!board[xStart][yStart] || !board[xFinish][yFinish]) {
+      return false
+    }
+  }
   return (
     <div>
       <div style={{
@@ -44,7 +106,10 @@ function Game() {
                 height: `${CELLS}px`,
                 backgroundColor: setCellColor(i,k),
                 border: '1px solid black'
-              }}></div>))
+              }}>
+                <div style={{width: `${CELLS}px`,height: `${CELLS}px`, fontSize:'60px', color: 'limegreen', pointerEvents:'none'}}>{board[i][k].player ? board[i][k].player: ''}</div>
+                {/* <img src={diamond} style={{width: `${CELLS}px`,height: `${CELLS}px`}}></img> */}
+              </div>))
         }
       </div>
     </div>
